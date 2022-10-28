d3180ca97e876a6f5e20b6c21e778dc96c19f1ab
akbarimo <mo.akbari15@gmail.com>
2022-10-28 15:36:59 -0700

    Completed sets

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,53 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // // check if item is already inside set
+  // var newKey = this['storage'];
+  // if (this.contains(item)) {
+  //   //   if true, return
+  //   return;
+  //   // else
+  // } else {
+  //   //  add property to set, where storage is the key and item is the value
+  //   this[newKey] = item;
+  // }
+  // this['storage']++;
+  if (!this.storage.includes(item)) {
+    this.storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  // loop through set
+  // _.each(this, function (element, i, collection) {
+  //   if (element === item) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // });
+  return this.storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  // save key or index position
+  // loop through set
+  //   check if set[key] === value
+  //      delete set[key]
+  // loop through set
+  //   if key > saved key
+  //     decrement key
+  return this.storage = this.storage.filter(function (element) {
+    if (!element === item) {
+      return element;
+    }
+  });
 };
 
 /*
