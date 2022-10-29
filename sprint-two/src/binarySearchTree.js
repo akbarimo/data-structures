2e18a4526762f7730b2ba014446f36232248e856
akbarimo <mo.akbari15@gmail.com>
2022-10-29 12:05:05 -0700

    Completed Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,77 @@
 var BinarySearchTree = function(value) {
-  // prototypal
+
+  const someInstance = Object.create(BinarySearchTree.prototype);
+
+  someInstance.value = value;
+  someInstance.left = null;
+  someInstance.right = null;
+
+  return someInstance;
 };
+
+BinarySearchTree.prototype.insert = function(value) {
+
+  const treeSearch = function (currentNode) {
+    if (currentNode.value > value) {
+      if (currentNode.left === null) {
+        currentNode.left = BinarySearchTree(value);
+        return;
+      } else {
+        treeSearch(currentNode.left);
+      }
+    } else if (currentNode.value < value) {
+      if (currentNode.right === null) {
+        currentNode.right = BinarySearchTree(value);
+        return;
+      } else {
+        treeSearch(currentNode.right);
+      }
+    }
+  };
+  treeSearch(this);
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+
+  let isFound = false;
+
+  const searchTree = function(currentNode) {
+    if (currentNode.value === value) {
+      return isFound = true;
+    }
+    if (currentNode.value < value) {
+      if (currentNode.right !== null) {
+        searchTree(currentNode.right);
+      }
+    } else if (currentNode.value > value) {
+      if (currentNode.left !== null) {
+        searchTree(currentNode.left);
+      }
+    }
+  };
+  searchTree(this);
+
+  return isFound;
+};
+
+// depthFirstLog function
+BinarySearchTree.prototype.depthFirstLog = function(cb, currentNode) {
+  const iterateTree = function(currentNode) {
+    cb(currentNode.value);
+    if (currentNode.left === null && currentNode.right === null) {
+      return;
+    }
+    if (currentNode.left !== null) {
+      iterateTree(currentNode.left);
+    }
+    if (currentNode.right !== null) {
+      iterateTree(currentNode.right);
+    }
+  };
+  iterateTree(this);
+
+};
+
 
 
 /*
